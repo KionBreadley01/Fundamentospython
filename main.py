e7825e0a8b366f0e2a1cbd904bd5eae22e010b73
Juan Manuel <juansanchezjuan125@gmail.com>
2023-06-08 15:18:06 -0500

    Iniciando con python

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Este es un string", "Hola Mundo")
+print("Este es un entero ",36)
+print(" Este es un float",3.14)
+print("Este es un  booleano",True)
+
+
+print(Type(True))
+print(Type(3.24))
+print(Type(3))
