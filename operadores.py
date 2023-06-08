8970f9a077e91d768f19bbd4bad16b4c17d11a77
Juan Manuel <juansanchezjuan125@gmail.com>
2023-06-08 15:42:46 -0500

    03 operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#Operarodes matematicos 
+#+ - * %
+#/ Division Flotante
+#// Divison entera
+#** potencia
+
+
+print(24 + 52)
+print(50 - 32)
+print(35 % 3)
+print(18 / 3)
+print(10 // 3)
+print(3** 3)
+
+
+
+
+print("Hola a" + "Todos")
+print("DSM 3A" *4 )
