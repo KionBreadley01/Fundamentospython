d036c37f3522ee8f79ae74cac1c37ba4a449226c
Juan Manuel <juansanchezjuan125@gmail.com>
2023-06-14 14:59:28 -0500

    08 listas

---
+++ listas.py
@@ -0,0 +1,26 @@
+#coleciones+
+#" listas
+#{item1, item2, item_n}
+my_string =[ 1,"dos",3.14, True]
+print(type(my_string ))
+
+my_string .append("new item")
+my_string .remove(1)
+print(my_string .pop())
+
+print(my_string [2])
+my_string ="calor valor"
+print(my_string )
+
+other_list = [3, 1,  4, 8, 2, 6, 4, ]
+print(other_list.reverse())
+other_list.reverse()
+print(other_list.sort())
+other_list.sort()
+
+print(other_list[:3])
+print(other_list[3:])
+
+
+
+
