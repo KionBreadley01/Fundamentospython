92e88aade69cc183bfa461f6cf14aa8cf887392c
Juan Manuel <juansanchezjuan125@gmail.com>
2023-06-14 15:32:15 -0500

    09 listas y crus

---
+++ crus.py
@@ -0,0 +1,27 @@
+comidas=[]
+def show_comidas():
+    for comida in comidas:
+        print(f"comida{comida}")
+
+def add_comida(comida):
+    comidas.append(comida)
+
+def del_comida(comida):
+    comidas.remove(comida)
+text_menu='''
+
+Elige una opcion:
+1- Agregar comida
+2- Eliminar comida
+3- mostra comidas 
+4- salir 
+'''
+
+
+
+while True:
+    choice_user = int(input(text_menu))
+    if choice_user== 1: 
+        comida = input("Escribe una comida: ")
+        add_comida(comida)
+
